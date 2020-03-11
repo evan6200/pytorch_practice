e53b5653cc648c240bb8bebda341f3e68f28a3e2
evan6200 <lover0107@gmail.com>
2020-03-11 17:57:43 +0800

    0322 update multi-personal tracking mechanism 0311 update
    0303_test_paper_modieifed_1D.py
    1. conv1d convolution.
    2. softmax
    3. cross entropy
    
    0303_test_paper.py
    1. This is used to be a material for paper.
    
    0303_evan_demo_3m.py
    1 conv1d struct for demo, not finish yet

--- 0.cut_mp4.py
+++ 0.cut_mp4.py
@@ -2,26 +2,14 @@ import cv2
 import numpy as np
 import sys
 
-print ('Number of arguments:', len(sys.argv), 'arguments.')
-print ('Argument List:', str(sys.argv))
-print ('usage:')
-print ('0.preview_mp4.py video.avi png_folder/ label')
 
-#video_workspace='/home/evan/mp4_to_png/'
-#png_data_folder=sys.argv[2] + '/'
-#label=sys.argv[3]
-#count=0
-#prefix_name="pic"
-#prefix_name=label+"_"+prefix_name
-# Create a VideoCapture object and read from input file
-# If the input is the camera, pass 0 instead of the video file name
-video_path=sys.argv[1]
-video_path='/home/evan/gopro_pic/2019.11.31/GOPR0102.MP4'
+video_path="/home/evan/gopro_pic/2019.12.19/3M_training.MP4"
+#video_path="/home/evan/gopro_pic/2019.11.31/15M.MP4"
 cap = cv2.VideoCapture(video_path)
-
+print ('test')
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 
-out = cv2.VideoWriter('output_1.avi', fourcc, 30, (1920,1080))
+out = cv2.VideoWriter('TEST.avi', fourcc, 30, (1920,1080))
  
 # Check if camera opened successfully
 if (cap.isOpened()== False): 
@@ -42,7 +30,7 @@ while(cap.isOpened()):
   ret, frame = cap.read()
   if ret == True:
     # Display the resulting frame
-#    cv2.imshow('Frame',frame)
+      cv2.imshow('Frame',frame)
 #    if png_data_folder != "" :
       count=count+1
 #        count_str='%05d'% count
@@ -52,7 +40,6 @@ while(cap.isOpened()):
         out.write(frame)
       if count > end :
           break;
-
 # When everything done, release the video capture object
 cap.release()
 print ("done" )
